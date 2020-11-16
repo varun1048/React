12f451b727b07b7cd4e1834b36599539009d6c05
varun <59528216+varun1048@users.noreply.github.com>
2020-11-16 14:02:10 +0530

    tested

---
+++ apptest/src/Test.js
@@ -0,0 +1,10 @@
+import output from 'react'
+import './Test.css'
+
+class Test extends output.Component{
+    render(){
+        return <button >varun</button>
+    }
+}
+
+export default Test
---
+++ apptest/src/index.js
@@ -0,0 +1,4 @@
+import reactdom from 'react-dom'
+import Test from './Test'
+
+reactdom.render(<Test />,document.getElementById('root'))
